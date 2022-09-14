4db06a54c277cd54245328b913b2c7d10ae72718
George Kreye <george.kreye@du.edu>
2022-09-14 15:34:49 -0600

    Finish SimplePlayerControl, add Counter

--- Labs/Assets/Scripts/SimplePlayerControl.cs
+++ Labs/Assets/Scripts/SimplePlayerControl.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SimplePlayerControl : MonoBehaviour
 {
 
     public float moveSpeed; // Increment to move by per frame
-    private float currentH = 0f; // Current horizontal movement
-    private float currentV = 0f; // Current vertical movement
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +17,30 @@ public class SimplePlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Check for player input and get any directions of movement
-        currentH = Input.GetAxis("Horizontal");
-        currentV = Input.GetAxis("Vertical");
+        // Get keyboard
+        var keyboard = Keyboard.current;
 
-        // Apply movement
-        transform.position = new Vector3();
+        // Check if directionals were pressed and move accordingly;
+        // allow canceling & combination of inputs
+        if (keyboard != null && keyboard.wKey.isPressed) // Up
+        {
+            transform.position = new Vector3(transform.position.x,
+                transform.position.y + moveSpeed);
+        }
+        if(keyboard != null && keyboard.aKey.isPressed) // Left
+        {
+            transform.position = new Vector3(transform.position.x -
+                moveSpeed,transform.position.y);
+        }
+        if (keyboard != null && keyboard.sKey.isPressed) // Down
+        {
+            transform.position = new Vector3(transform.position.x,
+                transform.position.y - moveSpeed);
+        }
+        if (keyboard != null && keyboard.dKey.isPressed) // Right
+        {
+            transform.position = new Vector3(transform.position.x +
+                moveSpeed, transform.position.y);
+        }
     }
 }

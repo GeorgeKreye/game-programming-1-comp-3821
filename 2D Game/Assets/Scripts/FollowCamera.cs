9a42b49d1d25e2b393bf6957c2862bfdc6263091
George Kreye <george.kreye@du.edu>
2022-09-29 11:07:35 -0600

    some tweaks to camera following

--- 2D Game/Assets/Scripts/FollowCamera.cs
+++ 2D Game/Assets/Scripts/FollowCamera.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     // Target Game Object to follow
-    [Tooltip("The Game Object the camera should follow")]
+    [Tooltip("The GameObject the camera should follow")]
     [SerializeField]
     private GameObject target;
 
     // Offset to follow at
-    [Tooltip("The offset from the center of the GameObject that the camera should follow at")]
+    [Tooltip("The offset from the center of the GameObject that the camera " +
+        "should follow at")]
     [SerializeField]
     private Vector2 offset;
 
@@ -22,6 +23,10 @@ public class FollowCamera : MonoBehaviour
     {
         // Get transform to track target object's location
         targetLocation = target.GetComponent<Transform>();
+
+        // Set initial position
+        transform.position = new Vector3(targetLocation.position.x + offset.x,
+            targetLocation.position.y + offset.y, transform.position.z);
     }
 
     // Update is called once per frame
@@ -29,6 +34,6 @@ public class FollowCamera : MonoBehaviour
     {
         // Update position
         transform.position = new Vector3(targetLocation.position.x + offset.x,
-            targetLocation.position.y + offset.y,transform.position.z);
+            targetLocation.position.y + offset.y, transform.position.z);
     }
 }

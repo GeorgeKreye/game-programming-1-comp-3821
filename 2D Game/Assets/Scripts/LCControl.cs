65d0d0c2204a29dfba4b4ac1e69cc81f08478132
George Kreye <george.kreye@du.edu>
2022-09-30 11:32:40 -0600

    More animations, start controls

---
+++ 2D Game/Assets/Scripts/LCControl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LCControl : MonoBehaviour
+{
+
+    [SerializeField]
+    private float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}

f703ad86147185225d289e15ce4f22e7f55a22d0
George Kreye <george.kreye@du.edu>
2022-10-01 16:48:16 -0600

    Start Lab5

---
+++ Labs/Assets/Scripts/KillMethod.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillMethod : MonoBehaviour
+{
+    public void Kill()
+    {
+        // Print to console
+        Debug.Log("Player killed");
+    }
+}

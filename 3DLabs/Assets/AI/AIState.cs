50b9a9c41d3e0241edfa4fe7a75d227426fecbad
George Kreye <george.kreye@du.edu>
2022-11-05 15:43:44 -0600

    Start AI scripts

---
+++ 3DLabs/Assets/AI/AIState.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Scriptable Object abstract class for AI states
+/// </summary>
+public abstract class AIState : ScriptableObject
+{
+    /// <summary>
+    /// Perform action(s) associated with this AI state
+    /// </summary>
+    /// <param name="controller">The AI Controller calling Act</param>
+    public abstract void Act(AIStateController controller);
+}
--- 3DLabs/Assets/AI/AIStateController.cs
+++ 3DLabs/Assets/AI/AIStateController.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+/// <summary>
+/// Class for an AI Agent to handle AI states
+/// </summary>
 public class AIStateController : MonoBehaviour
 {
+    [Header("State Control")]
+    [Tooltip("The current state the context is in")]
+    public AIState currentState;
+
     // Start is called before the first frame update
     void Start()
     {

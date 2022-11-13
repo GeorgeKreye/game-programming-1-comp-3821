b75de24823504f1390678c8ae8cee40f3d6d0f80
George Kreye <george.kreye@du.edu>
2022-11-13 15:17:22 -0700

    Start interactables

---
+++ 3D Platformer/Assets/Interactable/Interact Behaviors/CompositeInteractableSO.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Interactable SO for performing multiple interaction behaviors at once
+/// </summary>
+public class CompositeInteractableSO : InteractableSO
+{
+    [Tooltip("The Interactable SOs used in this composite interactable")]
+    [SerializeField] private InteractableSO[] interactables;
+
+    // Performs interact behaviors
+    public override void InteractBehavior(CharacterInteractManager manager,
+        CharacterController controller)
+    {
+        // Loop through interactable SOs
+        foreach(InteractableSO interactable in interactables)
+        {
+            // Perform behaviors
+            interactable.InteractBehavior(manager, controller);
+        }
+    }
+}
---
+++ 3D Platformer/Assets/Interactable/ModularInteractable.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script for performing interactable behavior using scriptable objects
+/// </summary>
+public class ModularInteractable : MonoBehaviour, IInteractable
+{
+    [Tooltip("The interactable scriptable object to use")]
+    [SerializeField] private InteractableSO interactBehavior;
+
+    // Perform interaction
+    public void Interact(CharacterInteractManager manager,
+        CharacterController controller)
+    {
+        interactBehavior.InteractBehavior(manager, controller);
+    }
+}

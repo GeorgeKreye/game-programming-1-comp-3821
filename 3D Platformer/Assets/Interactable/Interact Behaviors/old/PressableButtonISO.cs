be2fe85177f6cd4ccecf2a31426d7a4c008f2f3c
George Kreye <george.kreye@du.edu>
2022-11-19 13:35:13 -0700

    reimporting old lab ISOs since new methods don't work for some reason

---
+++ 3D Platformer/Assets/Interactable/Interact Behaviors/old/PressableButtonISO.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "InteractibleSO/PressableButton",
+    fileName = "PressableButtonISO")]
+public class PressableButtonISO : InteractableSO
+{ 
+    public override void InteractBehavior(CharacterInteractManager manager,
+        CharacterController controller)
+    {
+        Debug.Log("Trying to press button");
+        manager.GetActiveInteractable().GetComponent<PressableButtonO>()
+            .Press();
+    }
+}
